766a61e848eb2059ca39ae22c0321086d9b6974b
abhinavmodem <121121021+abhinavmodem@users.noreply.github.com>
2023-05-24 21:32:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,51 +5,38 @@ app = Flask(__name__)
 
 # MySQL connection configuration
 db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'abhinav1',
-    'database': 'test'
+    'host': 'abhinavmodem.mysql.pythonanywhere-services.com',
+    'user': 'abhinavmodem',
+    'password': '@Abhi2004',
+    'database': 'abhinavmodem$test'
 }
-@app.route('/')
-def get_test_word():
-    # Connect to the MySQL database
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
     conn = mysql.connector.connect(**db_config)
     cursor = conn.cursor()
 
-    # Retrieve the "Test" word from the database
+    if request.method == 'POST':
+        new_word = request.form.get('new_word')
+        query = 'UPDATE test_table SET test_word = %s;'
+        cursor.execute(query, (new_word,))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return 'Word updated successfully'
+
     query = 'SELECT test_word FROM test_table LIMIT 1;'
     cursor.execute(query)
     result = cursor.fetchone()
-
-    # Close the database connection
     cursor.close()
     conn.close()
 
     if result:
-        return result[0]
+        test_word = result[0]
     else:
-        return 'No test word found'
-@app.route('/admin', methods=['GET', 'POST'])
-def admin_portal():
-    if request.method == 'POST':
-        new_word = request.form.get('new_word')
+        test_word = 'No test word found'
 
-        # Connect to the MySQL database
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor()
+    return render_template('admin.html', test_word=test_word)
 
-        # Update the "Test" word in the database
-        query = 'UPDATE test_table SET test_word = %s;'
-        cursor.execute(query, (new_word,))
-        conn.commit()
-
-        # Close the database connection
-        cursor.close()
-        conn.close()
-
-        return 'Word updated successfully'
-
-    # Render the admin portal template
-    return render_template('admin.html')
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
